b9888689eda8ec24f95e49b07368872d920fc0e2
lear <1325925707@qq.com>
2017-06-15 10:34:36 +0800

    add Storage module

--- routes/Analy_x.js
+++ routes/Analy_x.js
@@ -1,10 +1,32 @@
 'use strict';
 var router = require('express').Router();
 
+var AV = require('leanengine');
+
+// AV.init({
+//   appId: 'XvM4C5TKB8AK1XcyC3041OeQ-gzGzoHsz',//process.env.LEANCLOUD_APP_ID,
+//   appKey: 'hF492dpV15UMdQt120SE966S',//process.env.LEANCLOUD_APP_KEY,
+// });
+
+var Novel = AV.Object.extend('Novel');
+// var Novel = Ax.Object.extend('Novel');
 
 router.get('/', function(req, res, next) {
     res.writeHead(200, {'Content-Type': 'text/json;charset=UTF-8'});
     res.end('你好，黑户。');
+    var novel = new Novel();
+    novel.set('name', '武林高手异界行');
+    novel.set('url', 'http://www.baidu.com');
+    novel.set('latestChapter', '第2012章 胖揍逆子');
+    novel.set('plantFormId', '2');
+    novel.set('author', '呆呆龙');
+    novel.set('type', '武侠');
+    novel.save().then(function() {
+        console.log('save success');
+    }, function(error) {
+        alert(JSON.stringify(error));
+    });
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,19 +2,35 @@
 var AV = require('leanengine');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  appId: 'XvM4C5TKB8AK1XcyC3041OeQ-gzGzoHsz',//process.env.LEANCLOUD_APP_ID,
+  appKey: 'hF492dpV15UMdQt120SE966S',//process.env.LEANCLOUD_APP_KEY,
+  masterKey: 'E4zmhkfMxtVIywSB4qnhkq1C'//process.env.LEANCLOUD_APP_MASTER_KEY
 });
+
+// var Novel = AV.Object.extend('Novel');
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
+
+    // var novel = new Novel();
+    // novel.set('name', '武林高手异界行');
+    // novel.set('url', 'http://www.baidu.com');
+    // novel.set('latestChapter', '第2012章 胖揍逆子');
+    // novel.set('plantFormId', '2');
+    // novel.set('author', '呆呆龙');
+    // novel.set('type', '武侠');
+    // novel.save().then(function() {
+    //     console.log('save success');
+    // }, function(error) {
+    //     alert(JSON.stringify(error));
+    // });
 
 var app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
 
 app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
